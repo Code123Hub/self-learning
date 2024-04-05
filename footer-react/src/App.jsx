4f972965f75ffd9d8bfe20c96011033e59fb12af
Code123Hub <codehub123@outlook.com>
2024-04-05 12:44:19 +0530

    add

---
+++ footer-react/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+import Footer from './Components/Footer/Footer'
+
+
+function App() {
+  return (
+    <div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
